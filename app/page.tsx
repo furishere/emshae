920045b555769efbb036c398470b701745b2144e
Hariom Mandal <hariommandal15@gmail.com>
2026-07-25 17:40:02 +0530

    keyboard done

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,318 @@
-import { Sun, Volume1, Volume2, VolumeX } from "lucide-react"
+"use client"
+import { ArrowBigDown, ArrowBigUp, ChevronDown, ChevronLeft, ChevronRight, ChevronUp, Command, Option } from "lucide-react"
+import { AudioLines, AudioWaveform, Circle, Grid2X2, ListVideo, Mic, Moon, ScanSearch, Search, Sun, SunDim, Volume1, Volume2, VolumeX } from "lucide-react"
+import { useState } from "react"
 
 export default function Home(){
     const Line1 = [{
-      icon : <Sun />,
+      icon : <SunDim className="h-3 w-3"/>,
       title : "F1"
     },{
-      icon : <Sun />,
+      icon : <Sun className="h-3 w-3"/>,
       title : "F2"
     },{
-      icon : <Sun />,
+      icon : <Grid2X2 className="h-3 w-3" />,
       title : "F3"
     },{
-      icon : <Sun />,
+      icon : <Search className="h-3 w-3"/>,
       title : "F4"
     },{
-      icon : <Sun />,
+      icon : <Mic className="h-3 w-3"/>,
       title : "F5"
     },{
-      icon : <Sun />,
+      icon : <Moon className="h-3 w-3"/>,
       title : "F6"
     },{
-      icon : <Sun />,
+      icon : <AudioLines className="h-3 w-3"/>,
       title : "F7"
     },{
-      icon : <Sun />,
+      icon : <AudioWaveform className="h-3 w-3" />,
       title : "F8"
     },{
-      icon : <Sun />,
+      icon : <ListVideo className="h-3 w-3" />,
       title : "F9"
     },{
-      icon : <VolumeX />,
+      icon : <VolumeX className="h-3 w-3"/>,
       title : "F10"
     },{
-      icon : <Volume1 />,
+      icon : <Volume1 className="h-3 w-3"/>,
       title : "F11"
     },{
-      icon : <Volume2 />,
+      icon : <Volume2 className="h-3 w-3"/>,
       title : "F12"
-    },]
+    },{
+      icon : <Circle className="text-gray-200 text-center"/>,
+    }]
 
-    const alphabate = ["Q","W","E","R","T","Y","U","I","O","P"]
-    const alphateLin2 = ["A","S","D","F","G","H","J","K","L"]
+    const Line2 = [{
+      icon : "~",
+      title : "`"
+    },{
+      icon : "!",
+      title : "1"
+    },{
+      icon : "@",
+      title : "2"
+    },{
+      icon : "#",
+      title : "3"
+    },{
+      icon : "$",
+      title : "4"
+    },{
+      icon : "%",
+      title : "5"
+    },{
+      icon : "^",
+      title : "6"
+    },{
+      icon : "&",
+      title : "7"
+    },{
+      icon : "*",
+      title : "8"
+    },{
+      icon : "(",
+      title : "9"
+    },{
+      icon : ")",
+      title : "0"
+    },{
+      icon : "_",
+      title : "-"
+    },{
+      icon : "+",
+      title : "="
+    }]
 
-  return <div className="flex justify-center items-center">
-    <div className="w-full max-w-xl flex flex-col gap-1 mt-24 mx-auto">
-     <div className="flex gap-1">
-      <Alphabate size="md" text="tab"/>
-        {alphabate.map(alpha => (
-          <Alphabate text={alpha} key={alpha} size="sm"/>
+    const Line3 = [
+      {
+        title : "Q"
+      },
+      {
+        title : "w"
+      },
+      {
+        title : "E"
+      },
+      {
+        title : "R"
+      },
+      {
+        title : "T"
+      },
+      {
+        title : "Y"
+      },
+      {
+        title : "U"
+      },
+      {
+        title : "I"
+      },
+      {
+        title : "O"
+      },
+      {
+        title : "P"
+      },{
+        title : "{",
+        icon : "["
+      },{
+        title : "}",
+        icon : "]"
+      }, {
+        title : "|",
+      }
+    ]
+  
+    const Line4 = [
+      {
+        title : "A"
+      },
+      {
+        title : "S"
+      },
+      {
+        title : "D"
+      },
+      {
+        title : "F"
+      },
+      {
+        title : "G"
+      },
+      {
+        title : "H"
+      },
+      {
+        title : "J"
+      },
+      {
+        title : "K"
+      },
+      {
+        title : "L"
+      },
+      {
+        title : ";",
+        icon : ":"
+      },
+      {
+        title : "'",
+        icon : '"'
+      }
+    ]
+
+    const Line5 = [
+      {
+        title : "Z"
+      },
+      {
+        title : "X"
+      },
+      {
+        title : "C"
+      },
+      {
+        title : "V"
+      },
+      {
+        title : "B"
+      },
+      {
+        title : "N"
+      },
+      {
+        title : "M"
+      },
+      {
+        title : "<",
+        icon : ","
+      },
+      {
+        title : ">",
+        icon : "."
+      },
+      {
+        title : "?",
+        icon : "/"
+      },
+    ]
+
+    const Line6A = [
+      {
+        title : "fn",
+        icon : <Sun  className="h-3 w-3"/>
+      },
+      {
+        title : "control",
+        icon : "^"
+      },
+      {
+        title : "option",
+        icon : <Option className="h-3 w-3"/>
+      },
+      {
+        title : "command",
+        icon : <Command className="h-3 w-3"/>
+      },
+    ]
+    const Line6B = [
+       {
+        title : "control",
+        icon : "^"
+      },
+      {
+        title : "option",
+        icon : <Option className="h-3 w-3"/>
+      },
+      {
+        icon : <ChevronLeft  className="h-3 w-3"/>
+      },
+      {
+        icon : <ChevronUp className="h-3 w-3"/>,
+        title : " "
+      },{
+        icon : <ChevronRight className="h-3 w-3"/>
+      }
+    ]
+
+
+  return <div className="flex flex-row justify-center items-center">
+
+    
+    <div className="flex justify-center flex-col gap-2 mt-24 mx-auto bg-gray-100 p-4 rounded-xl inset-shadow-sm">
+
+      {/* Line 1 */}
+      <div className="flex gap-2 justify-center">
+      <Alphabate size="md" text="esc"/>
+        {Line1.map(line => (
+          <Alphabate text={line.title} icon={line.icon} key={line.title} size="sm"/>
         ))}
       </div>
-     <div className="flex gap-1">
+
+
+      {/* Line 2 */}
+     <div className="flex gap-4 justify-center">
+        {Line2.map(line => (
+          <Alphabate text={line.title} key={line.title} icon={line.icon} size="sm"/>
+        ))}
+        <Alphabate size="md" text="delete"/>
+      </div>
+
+      {/* Line 3 */}
+     <div className="flex gap-4.5 justify-center">
       <Alphabate size="md" text="tab"/>
-        {alphabate.map(alpha => (
-          <Alphabate text={alpha} key={alpha} size="sm"/>
+        {Line3.map(line => (
+          <Alphabate text={line.title} icon={line.icon} key={line.title} size="sm"/>
         ))}
       </div>
-     <div className="flex gap-1">
+
+      {/* linr 4 */}
+     <div className="flex gap-4.5 justify-center">
       <Alphabate size="md" text="caps"/>
-        {alphateLin2.map(alpha => (
-          <Alphabate text={alpha} key={alpha} size="sm"/>
+        {Line4.map(line => (
+          <Alphabate text={line.title} icon={line.icon} key={line.title} size="sm"/>
         ))}
         <Alphabate size="md" text="return"/>
       </div>
-     <div className="flex gap-1">
+
+     {/* Line 5 */}
+      <div className="flex gap-5.5 justify-center">
       <Alphabate size="lg" text="shift"/>
-        {alphateLin2.map(alpha => (
-          <Alphabate text={alpha} key={alpha} size="sm"/>
+        {Line5.map(line => (
+          <Alphabate text={line.title} icon={line.icon} key={line.title} size="sm"/>
         ))}
         <Alphabate size="lg" text="shift"/>
       </div>
+
+        {/* Line 6 */}
+        <div className="flex  gap-3 justify-center">
+        {Line6A.map(line => (
+          <Alphabate size="sm"  icon={line.icon} key={line.title}/>
+        ))}
+        <Alphabate size="xl"/>
+        {Line6B.map(line => (
+          <Alphabate size="sm" icon={line.icon} key={line.title}/>
+        ))}
+        </div>
     </div>
   </div>
 }
 
 interface ButtonProps {
-  text : string,
+  text? : string,
   icon? : React.ReactNode,
   size : "sm" | "md" | "lg" | "xl",
   textSize? : "sm" | "md" | "lg"
 }
 
 const sizes = {
-  sm : "px-5",
-  md : "px-10",
-  lg : "px-12",
-  xl : "px-32"
+  sm : "w-12",
+  md : "w-20 inset-x-0 text-3 bottom-text ",
+  lg : "w-24 bottom-text",
+  xl : "w-64"
 }
 
 
@@ -96,7 +323,18 @@ function Alphabate({
   textSize,
   icon
 }:ButtonProps){
-  return <div className={`py-3 inset-shadow-sm  bg-gray-2 rounded-lg border border-gray-200 cursor-pointer ${sizes[size]}`}>
-    {text}
-  </div>
+
+  const playSound = () => {
+      const audio  = new Audio("/sounds/click.mp3")
+      audio.play()
+  }
+
+  return <button className={`h-12 inset-shadow-sm  bg-gray-2 rounded-lg border border-gray-300 cursor-pointer items-center text-center transition transform duration-150 active:scale-95 active:inset-shadow-sm active:border-gray-400 ${sizes[size]}`}
+  onClick={playSound}>
+    <div className="flex flex-col items-center">
+    <div className="text-xs mb-1 mt-1">{icon}</div>
+    <div className="items-center text-[
+OKLCH(0.371 0 0)] align-bottom text-xs"> {text}</div>
+    </div>
+  </button>
 }
